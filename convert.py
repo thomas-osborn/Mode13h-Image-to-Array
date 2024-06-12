aa5aa7691a4d5cfa7915e6d5f9da95d0268592dc
ArcenVi <thomas.osborn91@gmail.com>
2024-06-12 15:47:23 +1000

    added processing status

--- convert.py
+++ convert.py
@@ -10,6 +10,9 @@ def closest(color):
     distances = np.sqrt(np.sum((colors-color)**2,axis=1))
     return np.where(distances == np.amin(distances))[0][0]
 
+done = False
+spinner = MoonSpinner('Processing… ')
+
 img_folder = 'image-holder'
 img_file = os.listdir(img_folder)[0]
 img_path = os.path.join(os.path.basename(img_folder), os.path.basename(img_file))
@@ -27,9 +30,6 @@ final_val = []
 w, h = img.size
 img_out = Image.new(mode='RGB', size=(w, h))
 
-done = False
-spinner = MoonSpinner('Processing… ')
-
 inc = 0
 for i in img_val:
     ndx = closest(i)
@@ -37,10 +37,9 @@ for i in img_val:
     xpos = inc % w
     ypos = inc // w
     img_out.putpixel((xpos, ypos), ref_val[ndx])
-    inc += 1
-
     if inc % 5000 == 0:
         spinner.next()
+    inc += 1
     
 done = True
 final = ', '.join(str(x) for x in final_val)
